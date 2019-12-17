7b9e21d4d96628f78133bf15890b1d45fa5aeee9
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2019-12-17 22:29:32 +0100

    Packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='fluxify',
+    version='0.0.1',
+    author='Ibragim Abubakarov',
+    author_email='ibragim.ai95@gmail.com',
+    description='A micro python package that can retrieve and flow control data from almost any type of file.',
+    long_description=long_description,
+    url='https://github.com/ibragim64/fluxify',
+    packages=['fluxify'],
+    install_requires=['pandas'],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
